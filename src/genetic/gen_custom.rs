05c1c8c389af4d82b75887535fefb460a9a01c2d
Giovanni-Tedesco <giotedesco33@gmail.com>
2021-05-25 15:11:09 -0400

    Changes to GenCustom

--- src/genetic/gen_custom.rs
+++ src/genetic/gen_custom.rs
@@ -5,7 +5,7 @@
 /// The downside to this is that the genetic algorithm will have to be implemented
 /// by hand. However, it can leverage the existing selection methods. Since those
 /// are not reliant on the Genetic trait defined in mod.rs
-pub trait GeneticCustom<T, Chromosome> {
+pub trait GeneticCustom<Chromosome> {
     fn gene(&self) -> Chromosome;
 
     fn from_gene(chromosome: &Chromosome) -> Self;
